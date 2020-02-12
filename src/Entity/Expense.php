a2197d226c4aaf088cb81c1feeabd03cd5dc9080
Matthieu Leorat <matthieu.leorat@smile.fr>
2020-02-12 12:58:32 +0100

    Add missing __toString method on expend entity

--- src/Entity/Expense.php
+++ src/Entity/Expense.php
@@ -52,6 +52,11 @@ class Expense
      */
     private $comment;
 
+    public function __toString()
+    {
+        return $this->label;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
