21231a7a3ea5330853a7ef3e35bebf430588de4c
Matthieu Leorat <matthieu.leorat@smile.fr>
2020-03-10 14:13:39 +0100

    Set hte equal case for the categry guesser

--- src/CategoryGuesser/CategoryGuesser.php
+++ src/CategoryGuesser/CategoryGuesser.php
@@ -2,18 +2,64 @@
 
 namespace App\CategoryGuesser;
 
-use App\Entity\Category;
 use App\Entity\DetailsToCategory;
+use App\Entity\Filter;
+use App\Entity\Transaction;
 
 class CategoryGuesser
 {
-    public static function execute(DetailsToCategory $detailsToCategory, string $details) : ? Category
+    /**
+     * @var Transaction
+     */
+    private $transaction;
+    /**
+     * @var DetailsToCategory
+     */
+    private $detailToCategory;
+
+    public function execute(DetailsToCategory $detailsToCategory, Transaction $transaction) : bool
     {
-        preg_match("/{$detailsToCategory->getRegex()}/m", $details, $matches);
-        if (count($matches)) {
-            return $detailsToCategory->getCategory();
+        $this->detailToCategory = $detailsToCategory;
+        $this->transaction = $transaction;
+
+        foreach ($detailsToCategory->getFilters() as $filter) {
+            if (false === $this->applyFilter($filter)) {
+                return false;
+            }
         }
 
-        return null;
+        return true;
+    }
+
+    private function applyFilter(Filter $filter)
+    {
+        return $this->{$filter->getCompareOperator()}($filter);
+    }
+
+    private function startWith(Filter $filter)
+    {
+
+    }
+
+    private function endWith(Filter $filter)
+    {
+
+    }
+
+    private function contain(Filter $filter)
+    {
+
+    }
+
+    private function equal(Filter $filter) : bool
+    {
+        $needle = $filter->getValue();
+        $subject = $this->transaction->getTypeData($filter->getField());
+
+        if ($subject == $needle) {
+            return true;
+        }
+
+        return false;
     }
 }
