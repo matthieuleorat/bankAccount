80e3f1c0f678449a0d3b7b043acf010612df7be9
Matthieu Leorat <matthieu.leorat@smile.fr>
2020-03-12 13:24:11 +0100

    Add lvl attr on category checkbox

--- src/Form/ResultatType.php
+++ src/Form/ResultatType.php
@@ -6,6 +6,7 @@ use App\Entity\Category;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -29,7 +30,10 @@ class ResultatType extends AbstractType
                 'multiple' => true,
                 'query_builder' => function (EntityRepository $er) {
                     return $er->getNodesHierarchyQueryBuilder();
-                }
+                },
+                'choice_attr' => function($choice, $key, $value) {
+                    return ['attr_lvl' => $choice->getLvl()];
+                },
             ])
 
             ->add('save', SubmitType::class, ['attr' => ['class' => 'btn btn-primary action-save']])
