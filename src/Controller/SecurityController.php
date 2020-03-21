b80090e4ef99ae7949f81f18be746da626ff6568
Matthieu Leorat <matthieu.leorat@pm.me>
2020-03-21 14:58:13 +0100

    Use easy admin login page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -18,12 +18,32 @@ class SecurityController extends AbstractController
              return $this->redirectToRoute('easyadmin');
          }
 
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
+            'error' => $error,
+            'last_username' => $lastUsername,
+
+            // OPTIONAL parameters to customize the login form:
+
+            // the string used to generate the CSRF token. If you don't define
+            // this parameter, the login form won't include a CSRF token
+            'csrf_token_intention' => 'authenticate',
+            // the URL users are redirected to after the login (default: path('easyadmin'))
+            'target_path' => $this->generateUrl('easyadmin'),
+            // the label displayed for the username form field (the |trans filter is applied to it)
+            'username_label' => "Votre nom d'utilisateur",
+            // the label displayed for the password form field (the |trans filter is applied to it)
+            'password_label' => 'Votre mot de passe',
+            // the label displayed for the Sign In form button (the |trans filter is applied to it)
+            'sign_in_label' => 'Connexion',
+            // the 'name' HTML attribute of the <input> used for the username field (default: '_username')
+            'username_parameter' => 'username',
+            // the 'name' HTML attribute of the <input> used for the password field (default: '_password')
+            'password_parameter' => 'password',
+        ]);
     }
 
     /**
