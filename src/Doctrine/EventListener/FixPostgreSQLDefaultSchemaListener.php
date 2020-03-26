02a92db2d16d99609eb36eb0d040633b70ab7b5a
Matthieu Leorat <matthieu.leorat@smile.fr>
2020-03-26 09:10:29 +0100

    Fix doctirne migration

---
+++ src/Doctrine/EventListener/FixPostgreSQLDefaultSchemaListener.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Doctrine\EventListener;
+
+use Doctrine\DBAL\Schema\PostgreSqlSchemaManager;
+use Doctrine\ORM\Tools\Event\GenerateSchemaEventArgs;
+
+/**
+ * Class FixPostgreSQLDefaultSchemaListener
+ *
+ * Fix a bug with doctrine migration: https://gist.github.com/vudaltsov/ec01012d3fe27c9eed59aa7fd9089cf7
+ */
+final class FixPostgreSQLDefaultSchemaListener
+{
+    public function postGenerateSchema(GenerateSchemaEventArgs $args): void
+    {
+        $schemaManager = $args
+            ->getEntityManager()
+            ->getConnection()
+            ->getSchemaManager();
+
+        if (!$schemaManager instanceof PostgreSqlSchemaManager) {
+            return;
+        }
+
+        foreach ($schemaManager->getExistingSchemaSearchPaths() as $namespace) {
+            if (!$args->getSchema()->hasNamespace($namespace)) {
+                $args->getSchema()->createNamespace($namespace);
+            }
+        }
+    }
+}
