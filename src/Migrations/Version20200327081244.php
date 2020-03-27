1096869e9e02827bfa61231831f04ec6c4e02dd6
Matthieu Leorat <matthieu.leorat@smile.fr>
2020-03-27 09:16:33 +0100

    Set expense name as text instead of string to keep line break

---
+++ src/Migrations/Version20200327081244.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200327081244 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE expense ALTER label TYPE TEXT');
+        $this->addSql('ALTER TABLE expense ALTER label DROP DEFAULT');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE expense ALTER label TYPE VARCHAR(255)');
+        $this->addSql('ALTER TABLE expense ALTER label DROP DEFAULT');
+    }
+}
