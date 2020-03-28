1de5638ef06bf89fc353117b75d1573b02270c16
Matthieu Leorat <matthieu.leorat@pm.me>
2020-03-28 15:12:46 +0100

    Create an action to create a debt from an expense

--- src/Controller/Admin/DebtController.php
+++ src/Controller/Admin/DebtController.php
@@ -1,10 +1,34 @@
-<?php
-
+<?php declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
+use App\Entity\Debt;
+use App\Entity\Expense;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\EasyAdminController;
 
-class DebtController
+class DebtController extends EasyAdminController
 {
+    public function createNewEntity()
+    {
+        $entityFullyQualifiedClassName = $this->entity['class'];
+        $entity = new $entityFullyQualifiedClassName();
 
+        $expenseId = (int) $this->request->query->get('expense_id');
+
+        return $this->fillDebtWithExpense($expenseId, $entity);
+    }
+
+    public function fillDebtWithExpense(int $expenseId, Debt $entity) : Debt
+    {
+        $expense = $this->em->getRepository(Expense::class)->findOneBy(['id' => $expenseId]);
+
+        if ($expense instanceof Expense) {
+            $entity->setAmount($expense->getCredit() + $expense->getDebit());
+            $entity->setComment($expense->getLabel());
+            $entity->setDate($expense->getDate());
+            $entity->addExpense($expense);
+        }
+
+        return $entity;
+    }
 }
