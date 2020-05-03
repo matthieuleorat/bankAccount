06d70a4209fe2ff94f7fb120599963ccdc8b3e24
Matthieu Leorat <matthieu.leorat@pm.me>
2020-05-03 13:59:00 +0200

    Improve category list in expense forms

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -3,30 +3,23 @@
 namespace App\Form;
 
 use App\Entity\Category;
-use Doctrine\ORM\EntityManagerInterface;
+use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CategoryType extends AbstractType
 {
-    /**
-     * @var \Doctrine\Persistence\ObjectRepository|\Gedmo\Tree\Entity\Repository\NestedTreeRepository
-     */
-    private $repo;
-
-    public function __construct(EntityManagerInterface $entityManager)
-    {
-        $this->repo = $entityManager->getRepository(Category::class);
-    }
-
     public function configureOptions(OptionsResolver $resolver)
     {
-        $choices = $this->repo->childrenHierarchy(null, false, []);
-        dump($choices);exit;
         $resolver->setDefaults([
             'class' => Category::class,
-            'choices' => $this->repo->getChildren(null, false, null, 'asc', true),
+            'query_builder' => static function (NestedTreeRepository $er) {
+                return $er->getNodesHierarchyQueryBuilder();
+            },
+            'choice_label' => static function(Category $choice) {
+                return str_repeat('-', $choice->getLvl()). ' ' . $choice->getName();
+            },
         ]);
     }
 
