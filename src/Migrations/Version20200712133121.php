b8cc82c91816fa5f04e3d58e282a5cf3f289ed85
Matthieu <matthieu.leorat@protonmail.com>
2020-07-12 16:09:54 +0200

    #53 Fix bad migration (#60)

--- src/Migrations/Version20200712133121.php
+++ src/Migrations/Version20200712133121.php
@@ -22,8 +22,6 @@ final class Version20200712133121 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('DROP SEQUENCE filter_id_seq CASCADE');
-        $this->addSql('DROP TABLE filter');
         $this->addSql('ALTER INDEX idx_7fc45f1d5df6c30d RENAME TO IDX_B61F9B815DF6C30D');
     }
 
@@ -32,8 +30,6 @@ final class Version20200712133121 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('CREATE SEQUENCE filter_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE filter (id INT NOT NULL, details_to_category_id INT NOT NULL, field VARCHAR(255) NOT NULL, compare_operator VARCHAR(255) NOT NULL, value VARCHAR(255) DEFAULT NULL)');
         $this->addSql('ALTER INDEX idx_b61f9b815df6c30d RENAME TO idx_7fc45f1d5df6c30d');
     }
 }
