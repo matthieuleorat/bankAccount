99161e0ee6066f77de40a1780af11c1bb78af73a
Matthieu Leorat <matthieu.leorat@smile.fr>
2020-08-31 13:33:21 +0200

    Filter category on budget in session

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -5,11 +5,17 @@ namespace App\Controller\Admin;
 use App\Entity\Budget;
 use App\Entity\Category;
 use App\Twig\BudgetExtension;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Orm\EntityRepository;
 
 class CategoryCrudController extends AbstractCrudController
 {
@@ -47,6 +53,21 @@ class CategoryCrudController extends AbstractCrudController
         } elseif (Crud::PAGE_EDIT === $pageName) {
             return [$name, $parent, $budget];
         }
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        $queryBuilder = $this->get(EntityRepository::class)->createQueryBuilder($searchDto, $entityDto, $fields, $filters);
+
+        $session = $this->get('session');
+        $budgetId = $session->get(BudgetExtension::BUDGET_ID_SESSION_KEY);
+        $rootAlias = $queryBuilder->getRootAlias();
+        $queryBuilder
+            ->andWhere($rootAlias.'.budget = :budgetId')
+            ->setParameter('budgetId', $budgetId)
+        ;
+
+        return $queryBuilder;
     }
 
     public function createEntity(string $entityFqcn) : Category
